0a791a025aa7a96292c864e5b0487f639a076865
kanai <kanai.lal.dutta@accenture.com>
2024-09-17 20:15:23 +0530

    Restructure module block

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
-module "netdevOps" {  
-  source = "./infra/"
+module "ec2" {  
+  source         = "./infra/ec2"
+  instanceType   = "t3.medium"
+}
+
+
+module vpc {
+  source               = "./infra/vpc"
+  vpc_cidr             = "10.0.0.0/16"
+  publicSubnet_cidr    = ["10.0.10.0/24", "10.180.0.0/24"]
+  port                 = "80"
+  internet_cidr        = "0.0.0.0/0" 
 }
--- netops.tfvars
+++
@@ -1,5 +0,0 @@
-instanceType         = "t3.medium"
-vpc_cidr             = "10.0.0.0/16"
-publicSubnet_cidr    = ["10.0.10.0/24", "10.180.0.0/24"]
-port                 = "80"
-internet_cidr        = "0.0.0.0/0" 
