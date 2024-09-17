d976294b0e6f9225ec7a3db628be056f586f5320
kanai <kanai.lal.dutta@accenture.com>
2024-09-17 21:59:07 +0530

    updated subnet CIDR

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ module "ec2" {
 module vpc {
   source               = "./infra/vpc"
   vpc_cidr             = "10.0.0.0/16"
-  publicSubnet_cidr    = ["10.0.10.0/24", "10.180.0.0/24"]
+  publicSubnet_cidr    = ["10.0.10.0/24", "10.0.20.0/24"]
   port                 = "80"
   internet_cidr        = "0.0.0.0/0" 
 }
